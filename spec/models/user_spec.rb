13af1bd4b91d31d1627cb559a7ee18ecec9f0231
enakata <edson.nakata@webtechbr.com>
2020-04-10 21:24:20 -0400

    Primeiros testes na model user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
+=begin
+  Testes em models normalmente são feitos para 
+  validações personalizados.
+=end
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is invalid if the level is not between 1 and 99" do
+    expect(User.create(nickname: 'Chronos', kind: :wizard, level: 100)).to_not be_valid
+  end
+
+  it "returns the correct hero title" do
+    user = User.create(nickname: 'Chronos', kind: :wizard, level: 1)
+    expect(user.title).to eq('wizard Chronos #1')
+  end
+
+  # pending "add some examples to (or delete) #{__FILE__}"
 end
