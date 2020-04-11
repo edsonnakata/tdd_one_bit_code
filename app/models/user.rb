31fb6c4c817341112a222c55cf300bf10335f0c0
enakata <edson.nakata@webtechbr.com>
2020-04-10 21:11:50 -0400

    Preparando o model user

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  enum kind: [:knight, :wizzard]
+  validates :level, numericality: { greater_than: 0, less_than_or_equal_to: 99 }
+
+  def title
+    "#{self.kind} #{self.nickname} ##{self.level}"
+  end
+
+end
